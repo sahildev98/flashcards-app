0e822606ada77b5365aed49d255daa6c3e0b748a
sahil <sahil.98@hotmail.com>
2023-12-10 15:00:49 +0000

    Created sampleflash array objs to import static data in the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-
 function App() {
 
   return (
     <>
-      
+      <h1>Hello world</h1>
     </>
-  )
+  );
 }
+
+// static data being used as placeholder 
+const sample_Flashcards = [
+  {
+    id: 1,
+    question: 'What is 4 + 4 ?', 
+    answer: '4', 
+    options: [2, 3, 5, 4]
+  },
+
+  {
+    id: 2, 
+    question: 'What is 5 + 5', 
+    answer: '10', 
+    options: [6, 7, 8, 9, 10]
+  }
+]
 
 export default App
